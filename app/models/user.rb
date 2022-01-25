bbffe9a05f12db19e6780786b7a53bffcda79393
Chris Wiker <cwiker7@gmail.com>
2022-01-25 17:10:46 -0500

    commit tree c37de9bb1465d0f0e6346abc964b0c672db3a4e9

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,3 +2,8 @@ class User < ActiveRecord::Base
   has_many :reviews
   has_many :games, through: :reviews
 end
+
+desc "Start the server"
+task :server do
+  exec "rerun -b 'rackup config.ru'"
+end
--- config/environment.rb
+++ config/environment.rb
@@ -7,4 +7,7 @@ require 'bundler/setup'
 Bundler.require(:default, ENV['RACK_ENV'])
 
 # Require in all files in 'app' directory
-require_all 'app'
+require_relative ".config/environment"
+
+run ApplicationController
+
